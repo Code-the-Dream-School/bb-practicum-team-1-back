883a1b7392e9dc81f001f98a299fdae66151d53f
larasodjati <108501073+larasodjati@users.noreply.github.com>
2023-03-19 14:08:30 -0700

    updated app.js

--- src/app.js
+++ src/app.js
@@ -1,35 +1,35 @@
-require('dotenv').config()
-require('express-async-errors')
+require("dotenv").config();
+require("express-async-errors");
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors')
-const favicon = require('express-favicon');
-const logger = require('morgan');
+const cors = require("cors");
+const favicon = require("express-favicon");
+const logger = require("morgan");
 
-// const mainRouter = require('./routes/mainRouter.js');
-const userRouter = require('./routes/user')
+const mainRouter = require("./routes/mainRouter.js");
+const userRouter = require("./routes/user");
 
-//authentication middleware 
-const authenticateUser = require('../middleware/authentication')
+//authentication middleware
+const authenticateUser = require("../middleware/authentication");
 
 //error handler
-const notFoundMiddleware = require('../middleware/not-found')
-const errorHandlerMiddleware = require('../middleware/error-handler')
+const notFoundMiddleware = require("../middleware/not-found");
+const errorHandlerMiddleware = require("../middleware/error-handler");
 
 // middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(logger('dev'));
-app.use(express.static('public'))
-// app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger("dev"));
+app.use(express.static("public"));
+app.use(favicon(__dirname + "/public/favicon.ico"));
 
 // routes
-// app.use('/api/v1', mainRouter);
-app.use('/api/v1', userRouter)
+app.use("/api/v1", mainRouter);
+app.use("/api/v1/user", userRouter);
 
-app.use(notFoundMiddleware)
-app.use(errorHandlerMiddleware)
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 module.exports = app;
