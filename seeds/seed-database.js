fa0689292e02e18f32f9cda735b42c19f7b22b85
Manizha-khorram <108601629+Manizha-khorram@users.noreply.github.com>
2023-03-28 02:28:36 -0400

    BACK-33 Clearing db, Seeding db, Adding book.json

--- seeds/seed-database.js
+++ seeds/seed-database.js
@@ -5,17 +5,19 @@ const connectDB = require('../db/connect')
 
 // load models
 const User = require('../models/User')
+const Book = require('../models/Book')
 
 //  connect to MongoDB
 connectDB(process.env.MONGO_URI)
 
 // read the json file
 const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'))
-
+const books = JSON.parse(fs.readFileSync(`${__dirname}/books.json`, 'utf-8'))
 // seed data into the DB
 const seedData = async () => {
     try {
         await User.create(users)
+        await Book.create(books)
         console.log(`Data successfully seed into the database`.green.inverse)
         process.exit()
     } catch (err) {
